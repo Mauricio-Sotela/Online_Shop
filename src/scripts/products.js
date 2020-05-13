1e27e246eec7c0a386ffde4d3ed87fed4f58155f
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-05-13 13:11:06 +0200

    order3

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,138 +1,138 @@
-const products=[
-    {
-        "name": "Roberto",
-        "image": "./img/pizza/0.jpg",
-        "id": 0,
-        "price": "13.49",
-        "description": [
-            " Roast beef",
-            " Bell peppers ",
-            " Mushrooms",
-            " Onions",
-            " Tomatoes",
-            " Marinara"
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-
-    },
-    {
-        "name": "Luigi",
-        "image": "./img/pizza/1.jpg",
-        "id":1,
-        "price": "11.99",
-        "description": [
-            " Jamon",
-            "  Bell peppers",
-            " Pepperocini",
-            " Olives",
-            " Mozzarella",
-            " Gouda",
-            " Marinara"
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Four Meat",
-        "image": "./img/pizza/2.jpg",
-        "id": 2,
-        "price": "13.99",
-        "description": [
-            " Ham",
-            " Pizza sauce",
-            " Chared pineapple",
-            " Cheddar",
-            " Jalapeno"
-         ],
-         "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Marinara",
-        "image": "./img/pizza/3.jpg",
-        "id": 3,
-        "price": "12.99",
-        "description": [
-            " Ham",
-            " Pizza sauce",
-            " Chared pineapple",
-            " Cheddar",
-            " Jalapeno"
-         ],
-         "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    } ,
-    {
-        "name": "Anchovy",
-        "image": "./img/pizza/4.jpg",
-        "id": 4,
-        "price": "14.99",
-        "description": [
-            " Ham",
-            " Chared pineapple",
-            " Cheddar",
-            " Jalapeno"
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Margarita",
-        "image": "./img/pizza/5.jpg",
-        "id": 5,
-        "price": "15.50",
-        "description": [
-            " Tomatoes",
-            " Pizza sauce",
-            " Basil",
-            " Cheddar",
-            " Jalapeno"
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Seafood",
-        "image": "./img/pizza/6.jpg",
-        "id": 6,
-        "price": "14.99",
-        "description": [
-            " omatoes",
-            " Pizza sauce",
-            " Calamari",
-            " Cheddar",
-            " Jalapeno"
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Veggie",
-        "image": "./img/pizza/7.jpg",
-        "id": 7,
-        "price": "10.99",
-        "description": [
-            " Tomatoes",
-            " Pizza sauce",
-            " Chared pineapple",
-            " Cheddar",
-            " Jalapeno",
-            " Eggplant",
-           
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    {
-        "name": "Cabanossi",
-        "image": "./img/pizza/8.jpg",
-        "id": 8,
-        "price": "14.99",
-        "description": [
-            " Tomato",
-            " Kabana",
-            " Garlic oil",
-            " Onions",
-            " Mozzarella",
-            
-        ],
-        "info": " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos consequuntur sint cupiditate eum modi molestias sed earum quidem inventore."
-    },
-    
-]
+const products = [
+  {
+    name: "Roberto",
+    image: "./img/pizza/0.jpg",
+    id: 0,
+    price: "13.49",
+    description: [
+      " Roast beef",
+      " Bell peppers ",
+      " Mushrooms",
+      " Onions",
+      " Tomatoes",
+      " Marinara",
+    ],
+    info:
+      " This delicious pizza is made for you from the tastiest ingredients Roast beef,Bell peppers,Mushrooms,Onions,Tomatoes,Marinara",
+  },
+  {
+    name: "Luigi",
+    image: "./img/pizza/1.jpg",
+    id: 1,
+    price: "11.99",
+    description: [
+      " Jamon",
+      "  Bell peppers",
+      " Pepperocini",
+      " Olives",
+      " Mozzarella",
+      " Gouda",
+      " Marinara",
+    ],
+    info:
+      " This delicious pizza is made for you from the tastiest ingredients Jamon Bell peppers Pepperocini,Olives,Mozzarella,Goud, Marinara.",
+  },
+  {
+    name: "Four Meat",
+    image: "./img/pizza/2.jpg",
+    id: 2,
+    price: "13.99",
+    description: [
+      " Ham",
+      " Pizza sauce",
+      " Chared pineapple",
+      " Cheddar",
+      " Jalapeno",
+    ],
+    info:
+      " This delicious pizza is made for you from the tastiest ingredients  Ham,Pizza sauce,Chared pineapple,Cheddar,Jalapeno",
+  },
+  {
+    name: "Marinara",
+    image: "./img/pizza/3.jpg",
+    id: 3,
+    price: "12.99",
+    description: [
+      " Ham",
+      " Pizza sauce",
+      " Chared pineapple",
+      " Cheddar",
+      " Jalapeno",
+    ],
+    info:
+      "This delicious pizza is made for you from the tastiest ingredients  Ham,Pizza sauce,Chared pineapple,Cheddar,Jalapeno.",
+  },
+  {
+    name: "Anchovy",
+    image: "./img/pizza/4.jpg",
+    id: 4,
+    price: "14.99",
+    description: [" Ham", " Chared pineapple", " Cheddar", " Jalapeno"],
+    info:
+      " This delicious pizza is made for you from the tastiest ingredients  Ham,Pizza sauce,Chared pineapple,Cheddar,Jalapeno.",
+  },
+  {
+    name: "Margarita",
+    image: "./img/pizza/5.jpg",
+    id: 5,
+    price: "15.50",
+    description: [
+      " Tomatoes",
+      " Pizza sauce",
+      " Basil",
+      " Cheddar",
+      " Jalapeno",
+    ],
+    info:
+      "This delicious pizza is made for you from the tastiest ingredients, tomato sauce and cheese peels",
+  },
+  {
+    name: "Seafood",
+    image: "./img/pizza/6.jpg",
+    id: 6,
+    price: "14.99",
+    description: [
+      " omatoes",
+      " Pizza sauce",
+      " Calamari",
+      " Cheddar",
+      " Jalapeno",
+    ],
+    info:
+      "This delicious pizza is made for you from the tastiest ingredients   omatoes,Pizza sauce,Calamari,Cheddar,Jalapeno.",
+  },
+  {
+    name: "Veggie",
+    image: "./img/pizza/7.jpg",
+    id: 7,
+    price: "10.99",
+    description: [
+      " Tomatoes",
+      " Pizza sauce",
+      " Chared pineapple",
+      " Cheddar",
+      " Jalapeno",
+      " Eggplant",
+    ],
+    info:
+      "This delicious pizza is made for you from the tastiest ingredients   Tomatoes,Pizza sauce,Chared pineapple,Cheddar,Jalapeno, Eggplant.",
+  },
+  {
+    name: "Cabanossi",
+    image: "./img/pizza/8.jpg",
+    id: 8,
+    price: "14.99",
+    description: [
+      " Tomato",
+      " Kabana",
+      " Garlic oil",
+      " Onions",
+      " Mozzarella",
+    ],
+    info:
+      "This delicious pizza is made for you from the tastiest ingredients   Tomato,Kabana,Garlic oil,Onions,Mozzarella.",
+  },
+];
 
 let data1 = JSON.stringify(products);
 export { data1 };
